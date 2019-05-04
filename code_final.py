470066089240b557206be4b1ff9bf92b5cefddb8
Anindya Harchowdhury <harchowdhuryanindya2@gmail.com>
2019-05-04 07:55:30 +0530

    Update code_final.py

--- code_final.py
+++ code_final.py
@@ -4,6 +4,31 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers
 
+# Batch normalization
+def batch_norm(inputs, phase_train, decay=0.9, eps=1e-5):
+    """Batch Normalization
+
+       Args:
+           inputs: input data(Batch size) from last layer
+           phase_train: when you test, please set phase_train "None"
+       Returns:
+           output for next layer
+    """
+    gamma = tf.get_variable("gamma", shape=inputs.get_shape()[-1], dtype=tf.float32, initializer=tf.constant_initializer(1.0))
+    beta = tf.get_variable("beta", shape=inputs.get_shape()[-1], dtype=tf.float32, initializer=tf.constant_initializer(0.0))
+    pop_mean = tf.get_variable("pop_mean", trainable=False, shape=inputs.get_shape()[-1], dtype=tf.float32, initializer=tf.constant_initializer(0.0))
+    pop_var = tf.get_variable("pop_var", trainable=False, shape=inputs.get_shape()[-1], dtype=tf.float32, initializer=tf.constant_initializer(1.0))
+    axes = range(len(inputs.get_shape()) - 1)
+
+    if phase_train != None:
+        batch_mean, batch_var = tf.nn.moments(inputs, axes)
+        train_mean = tf.assign(pop_mean, pop_mean * decay + batch_mean*(1 - decay))
+        train_var = tf.assign(pop_var, pop_var * decay + batch_var * (1 - decay))
+        with tf.control_dependencies([train_mean, train_var]):
+            return tf.nn.batch_normalization(inputs, batch_mean, batch_var, beta, gamma, eps)
+    else:
+        return tf.nn.batch_normalization(inputs, pop_mean, pop_var, beta, gamma, eps)
+    
 # Convolutional 3D layer definition
 def conv3DLayer(input_layer, input_channels, output_channels, height, width, length, stride=1, activation=tf.nn.relu, padding="SAME", name="", is_training=True, M_pool = tf.nn.max_pool3d):
     with tf.variable_scope("conv3D" + name):
@@ -23,12 +48,13 @@ def conv3DLayer(input_layer, input_channels, output_channels, height, width, len
 
     return bias
 
-# fully connected layer definition
+# fully connected layer definition along with drop out parameter
 def fully_connected(input_layer, shape, name="", is_training=True,dropout=tf.layers.dropout):
     with tf.variable_scope("fully" + name):
         kernel = tf.get_variable("weights", shape=shape, \
             dtype=tf.float32, initializer=tf.truncated_normal_initializer(stddev=0.01))
         fully = tf.matmul(input_layer, kernel)
+        # relu implementation
         fully = tf.nn.relu(fully)
         if dropout:
         drop = dropout(inputs=fully, rate=0.5, training=mode == tf.estimator.ModeKeys.TRAIN)
